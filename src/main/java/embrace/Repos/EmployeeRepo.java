6f324ea19c56c7c1fc4e3a7990a6dfccc977c01d
DoctorWater <malckov.schura2015@gmail.com>
2023-05-05 21:30:01 +0300

    bugfix: unnecessary swagger apis fixed

--- src/main/java/embrace/Repos/EmployeeRepo.java
+++ src/main/java/embrace/Repos/EmployeeRepo.java
@@ -4,6 +4,7 @@ import embrace.Entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
